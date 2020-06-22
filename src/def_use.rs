3f0ee819e2ce2f6f294e25ff1785b5e5f452ec0f
Taegyun Kim <k.taegyun@gmail.com>
2020-06-21 21:44:27 -0400

    Directly access id_name for Identifier, and use fmt::Display for Value struct.

--- src/def_use.rs
+++ src/def_use.rs
@@ -1,85 +1,98 @@
 use grammar::*;
 use std::collections::HashSet;
-use std::collections::HashMap;
 use tree_fold::TreeFold;
 
 pub struct DefUse<'a> {
-  pub known_nodes     : HashSet<&'a str>
+    pub known_nodes: HashSet<&'a str>,
 }
 
 impl<'a> DefUse<'a> {
-  pub fn new() -> DefUse<'a> {
-    DefUse {
-      known_nodes  : HashSet::new(),
+    pub fn new() -> DefUse<'a> {
+        DefUse {
+            known_nodes: HashSet::new(),
+        }
     }
-  }
 }
 
 impl<'a> TreeFold<'a> for DefUse<'a> {
-  fn visit_pattern(&mut self, tree : &'a Pattern) {
-    let from_node = &tree.from_node;
-    let to_node   = &tree.to_node;
-    self.known_nodes.insert(from_node.get_str());
-    self.known_nodes.insert(to_node.get_str());
-  }
-
-  fn visit_filter(&mut self, tree : &'a Filter) {
-    match &tree {
-      Filter::Label(node, _) | Filter::Property(node, _, _)
-      =>
-      { if (!self.known_nodes.contains(node.get_str())) { panic!("Node {:?} not defined.", node.get_str()); } }
+    fn visit_pattern(&mut self, tree: &'a Pattern) {
+        let from_node = &tree.from_node;
+        let to_node = &tree.to_node;
+        self.known_nodes.insert(from_node.id_name);
+        self.known_nodes.insert(to_node.id_name);
     }
-  }
+
+    fn visit_filter(&mut self, tree: &'a Filter) {
+        match &tree {
+            Filter::Label(node, _) | Filter::Property(node, _, _) => {
+                if !self.known_nodes.contains(node.id_name) {
+                    panic!("Node {:?} not defined.", node.id_name);
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::super::lexer;
-  use super::super::parser;
-  use super::DefUse;
-  use super::super::tree_fold::TreeFold;
+    use super::super::lexer;
+    use super::super::parser;
+    use super::super::tree_fold::TreeFold;
+    use super::DefUse;
 
-  fn run_def_use(input_program : &str) {
-    // Lexing
-    let tokens = & mut lexer::get_tokens(input_program);
-  
-    // parsing
-    let token_iter = & mut tokens.iter().peekable();
-    let parse_tree = parser::parse_prog(token_iter);
-    assert!(token_iter.peek().is_none(), "token_iter is not empty.");
-    println!("Parse tree: {:?}\n", parse_tree);
-  
-    // Check that identifiers are defined before use
-    let mut def_use = DefUse::new();
-    def_use.visit_prog(&parse_tree);
-  }
+    fn run_def_use(input_program: &str) {
+        // Lexing
+        let tokens = &mut lexer::get_tokens(input_program);
 
-  macro_rules! test_pass {
-    ($input_code:expr,$test_name:ident) => (
-      #[test]
-      fn $test_name() {
-        let input_program = $input_code;
-        run_def_use(input_program);
-      }
-    )
-  }
+        // parsing
+        let token_iter = &mut tokens.iter().peekable();
+        let parse_tree = parser::parse_prog(token_iter);
+        assert!(token_iter.peek().is_none(), "token_iter is not empty.");
+        println!("Parse tree: {:?}\n", parse_tree);
 
-  macro_rules! test_fail {
-    ($input_code:expr,$test_name:ident,$panic_msg:expr) => (
-      #[test]
-      #[should_panic(expected=$panic_msg)]
-      fn $test_name() {
-        let input_program = $input_code;
-        run_def_use(input_program);
-      }
-    )
-  }
+        // Check that identifiers are defined before use
+        let mut def_use = DefUse::new();
+        def_use.visit_prog(&parse_tree);
+    }
 
-  test_pass!(r"MATCH n-->m MATCH n-*>m", test_def_use_no_filter);
-  test_pass!(r"MATCH n-->m MATCH n-*>m WHERE n : Node", test_def_use_label_filter);
-  test_pass!(r"MATCH n-->m MATCH n-*>m WHERE n.a == 5", test_def_use_prop_filter);
-  test_fail!(r"MATCH n-->m MATCH n-*>m WHERE x.a == 5", test_fail_def_use_prop_filter,
-             "Node \"x\" not defined.");
-  test_fail!(r"MATCH n-->m MATCH n-*>m WHERE x : a", test_fail_def_use_label_filter,
-             "Node \"x\" not defined.");
+    macro_rules! test_pass {
+        ($input_code:expr,$test_name:ident) => {
+            #[test]
+            fn $test_name() {
+                let input_program = $input_code;
+                run_def_use(input_program);
+            }
+        };
+    }
+
+    macro_rules! test_fail {
+        ($input_code:expr,$test_name:ident,$panic_msg:expr) => {
+            #[test]
+            #[should_panic(expected=$panic_msg)]
+            fn $test_name() {
+                let input_program = $input_code;
+                run_def_use(input_program);
+            }
+        };
+    }
+
+    test_pass!(r"MATCH n-->m MATCH n-*>m", test_def_use_no_filter);
+    test_pass!(
+        r"MATCH n-->m MATCH n-*>m WHERE n : Node",
+        test_def_use_label_filter
+    );
+    test_pass!(
+        r"MATCH n-->m MATCH n-*>m WHERE n.a == 5",
+        test_def_use_prop_filter
+    );
+    test_fail!(
+        r"MATCH n-->m MATCH n-*>m WHERE x.a == 5",
+        test_fail_def_use_prop_filter,
+        "Node \"x\" not defined."
+    );
+    test_fail!(
+        r"MATCH n-->m MATCH n-*>m WHERE x : a",
+        test_fail_def_use_label_filter,
+        "Node \"x\" not defined."
+    );
 }
--- src/grammar.rs
+++ src/grammar.rs
@@ -1,64 +1,52 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
 pub struct Prog<'a> {
-  pub patterns : Patterns<'a>,
-  pub filters  : Filters<'a>
+    pub patterns: Patterns<'a>,
+    pub filters: Filters<'a>,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Patterns<'a> {
-  pub pattern_vector : Vec<Pattern<'a>>
+    pub pattern_vector: Vec<Pattern<'a>>,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Pattern<'a> {
-  pub from_node         : Identifier<'a>,
-  pub to_node           : Identifier<'a>,
-  pub relationship_type : Relationship
+    pub from_node: Identifier<'a>,
+    pub to_node: Identifier<'a>,
+    pub relationship_type: Relationship,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Filters<'a> {
-  pub filter_vector : Vec<Filter<'a>>
+    pub filter_vector: Vec<Filter<'a>>,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Filter<'a> {
-  Label(Identifier<'a>, Identifier<'a>), // xyz : Person
-  Property(Identifier<'a>, Identifier<'a>, Value) // xyz.abc == 5
+    Label(Identifier<'a>, Identifier<'a>),           // xyz : Person
+    Property(Identifier<'a>, Identifier<'a>, Value), // xyz.abc == 5
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Relationship {
-  Path(),
-  Edge()
+    Path(),
+    Edge(),
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Identifier<'a> {
-  pub id_name : &'a str,
+    pub id_name: &'a str,
 }
 
-impl<'a> Identifier<'a> {
-  pub fn get_str(&self) -> &str{
-    return self.id_name;
-  }
-}
-
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Value {
-  pub value : u32
+    pub value: u32,
 }
 
-impl Value {
-  pub fn get_string(&self) -> String {
-    return self.value.to_string();
-  }
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value.to_string())
+    }
 }
