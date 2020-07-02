9fb2fa304ae24515ba858023b966077b57d1a1e7
Taegyun Kim <k.taegyun@gmail.com>
2020-07-02 12:10:45 -0400

    Update to codegen to handle parallel edges.

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -18,12 +18,18 @@ impl<'a> TreeFold<'a> for CodeGen<'a> {
         if self.paths.is_empty() {
             self.paths.push(vec![from_node, to_node]);
         } else {
+            let mut inserted = false;
             for path in &mut self.paths {
                 if path[0] == to_node {
                     path.insert(0, from_node);
+                    inserted = true;
                 } else if path[path.len() - 1] == from_node {
                     path.push(to_node);
+                    inserted = true;
                 }
+            }
+            if !inserted {
+                self.paths.push(vec![from_node, to_node]);
             }
         }
     }
@@ -61,5 +67,18 @@ mod tests {
 
         assert_eq!(code_gen.paths.len(), 1);
         assert_eq!(code_gen.paths[0], vec!["a", "b", "c"]);
+    }
+
+    #[test]
+    fn test_codegen_parallel() {
+        let tokens: Vec<Token> = lexer::get_tokens(r"MATCH b-->c : x,a-->b : y,a-->d:z,");
+        let mut token_iter: Peekable<std::slice::Iter<Token>> = tokens.iter().peekable();
+        let parse_tree: Prog = parser::parse_prog(&mut token_iter);
+
+        let mut code_gen = CodeGen::new();
+        code_gen.visit_prog(&parse_tree);
+
+        assert_eq!(code_gen.paths.len(), 2);
+        assert_eq!(code_gen.paths, vec![vec!["a", "b", "c"], vec!["a", "d"]]);
     }
 }
