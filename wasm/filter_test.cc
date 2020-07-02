fafd4629e0e4af7c3a3a102eacf79c7a7c70430b
Taegyun Kim <k.taegyun@gmail.com>
2020-07-01 22:55:52 -0400

    copy bidi filter.

--- wasm/filter_test.cc
+++ wasm/filter_test.cc
@@ -1,39 +1,78 @@
+#include <numeric>
 #include <regex>
+#include <set>
 #include <string>
 
-#include "filter.pb.h"
-#include "google/protobuf/util/json_util.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-TEST(FilterTest, ParseName) {
-  Config config;
+TEST(FilterTest, MapUpdate) {
+  std::map<std::string, std::string> spans_to_headers;
 
-  google::protobuf::util::JsonParseOptions options;
-  options.case_insensitive_enum_parsing = true;
-  options.ignore_unknown_fields = false;
-  google::protobuf::util::JsonStringToMessage("{'name': 'productpage-v1'}",
-                                              &config, options);
+  spans_to_headers.insert(std::make_pair("a", "1"));
+  spans_to_headers.insert(std::make_pair("b", "2"));
 
-  EXPECT_EQ(config.name(), "productpage-v1");
+  ASSERT_EQ(2, spans_to_headers.size());
+  spans_to_headers.at("a") += "0";
+
+  ASSERT_EQ(spans_to_headers.at("a"), "10");
+
+  auto it = spans_to_headers.find("b");
+  it->second += "00";
+
+  ASSERT_EQ(spans_to_headers.at("b"), "200");
+
+  it = spans_to_headers.find("c");
+  ASSERT_EQ(it, spans_to_headers.end());
+  spans_to_headers.insert(std::make_pair("c", "300"));
+
+  ASSERT_EQ(spans_to_headers.at("c"), "300");
+
+  ASSERT_EQ(3, spans_to_headers.size());
+
+  it = spans_to_headers.find("c");
 }
 
-TEST(FilterTest, RegexTest) {
-  const std::regex base_regex(
-      ".*,.*RecommendationService.*,.*ProductCatalogService.*");
+TEST(FilterTest, SplitStr) {
+  std::string s{"a-b,a-c,a-d"};
+  std::regex delimiter(",");
+  std::sregex_token_iterator it{s.begin(), s.end(), delimiter, -1};
+  std::vector<std::string> words{it, {}};
+  EXPECT_THAT(words, testing::ElementsAre("a-b", "a-c", "a-d"));
 
-  ASSERT_TRUE(
-      std::regex_match("/product/3,/RecommendationService.ListRecommendations,/"
-                       "ProductCatalogService.ListProducts",
-                       base_regex));
-  ASSERT_TRUE(
-      std::regex_match("/cart,/RecommendationService.ListRecommendations,/"
-                       "ProductCatalogService.ListProducts",
-                       base_regex));
+  std::string result = std::accumulate(
+      words.begin(), words.end(), std::string(),
+      [](const std::string &a, const std::string &b) -> std::string {
+        return a + (a.length() > 0 ? "," : "") + "x-" + b;
+      });
 
-  ASSERT_FALSE(std::regex_match("/product/3/,/ProductCatalogService.GetProduct",
-                                base_regex));
-  ASSERT_TRUE(std::regex_match(
-      "/cart/checkout,/hipstershop.RecommendationService/ListRecommendations,/"
-      "hipstershop.ProductCatalogService/ListProducts",
-      base_regex));
+  EXPECT_EQ(result, "x-a-b,x-a-c,x-a-d");
+}
+
+TEST(FilterTest, SplitStrNoDelim) {
+  std::string s{"a-b"};
+  std::regex delimiter(",");
+  std::sregex_token_iterator it{s.begin(), s.end(), delimiter, -1};
+  std::vector<std::string> words{it, {}};
+  EXPECT_THAT(words, testing::ElementsAre("a-b"));
+
+  for (auto &w : words) {
+    w = "x-" + w;
+  }
+
+  std::string result = std::accumulate(
+      words.begin(), words.end(), std::string(),
+      [](const std::string &a, const std::string &b) -> std::string {
+        return a + (a.length() > 0 ? "," : "") + b;
+      });
+
+  EXPECT_EQ(result, "x-a-b");
+}
+
+TEST(FilterTest, SplitToSet) {
+  std::string s{"a-b,a-c,a-d"};
+  std::regex delimiter(",");
+  std::sregex_token_iterator it{s.begin(), s.end(), delimiter, -1};
+  std::set<std::string> words{it, {}};
+  EXPECT_THAT(words, testing::UnorderedElementsAre("a-b", "a-c", "a-d"));
 }
