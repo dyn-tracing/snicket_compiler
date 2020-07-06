6706f64f63bf41cb66d1c095c0b2e3083ef3081c
Taegyun Kim <k.taegyun@gmail.com>
2020-07-06 09:34:26 -0400

    Commit deployment related scripts and files.

---
+++ deployment/bookinfo.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Create Kubernetes ConfigMap with compiled filter.
+kubectl create cm -n default example-filter --from-file=bazel-bin/filter.wasm
+
+# Inject the wasm binary into all services by patching following annotations.
+kubectl patch deployment -n default productpage-v1 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+kubectl patch deployment -n default reviews-v1 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+kubectl patch deployment -n default reviews-v2 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+kubectl patch deployment -n default reviews-v3 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+kubectl patch deployment -n default details-v1 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+kubectl patch deployment -n default ratings-v1 p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/userVolume":"[{\"name\":\"wasmfilters-dir\",\"configMap\": {\"name\": \"example-filter\"}}]","sidecar.istio.io/userVolumeMount":"[{\"mountPath\":\"/var/local/lib/wasm-filters\",\"name\":\"wasmfilters-dir\"}]"}}}}}'
+
+# Insert WASM filter into HTTP-level filter chain
+kubectl apply -f filter-productpage-v1.yaml
+kubectl apply -f filter-reviews-v1.yaml
+kubectl apply -f filter-reviews-v2.yaml
+kubectl apply -f filter-reviews-v3.yaml
+kubectl apply -f filter-ratings-v1.yaml
+kubectl apply -f filter-details-v1.yaml
