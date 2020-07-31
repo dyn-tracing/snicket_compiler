6729eea0c89c1b879db21f301a43a05c73eb6f0c
Taegyun Kim <k.taegyun@gmail.com>
2020-07-31 15:46:44 -0400

    Split into util file

--- wasm/filter_test.cc
+++ wasm/filter_test.cc
@@ -1,103 +1,10 @@
-#include <map>
 #include <numeric>
-#include <queue>
 #include <regex>
 #include <set>
 #include <string>
 
-#include "treenode.pb.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
-// Returns true if TreeNode n2 has a subtree rooted at n2 which is ismorphic
-// to n1.
-bool isIsomorphic(const TreeNode &n1, const TreeNode &n2) {
-  for (const auto &pair : n1.properties()) {
-    if ((!n2.properties().contains(pair.first)) ||
-        (n2.properties().at(pair.first) != pair.second)) {
-      return false;
-    }
-  }
-
-  if (n1.children().size() > n2.children().size()) {
-    return false;
-  }
-
-  // NOTE: Doesn't handle the case where other_child is isomorphic to multiple
-  // child
-  for (int i = 0; i < n1.children().size(); ++i) {
-    const auto &child = n1.children()[i];
-    bool has_isomorphic_node = false;
-    for (int j = 0; j < n2.children().size(); ++j) {
-      const auto &other_child = n2.children()[j];
-      if (isIsomorphic(child, other_child)) {
-        has_isomorphic_node = true;
-      }
-    }
-    if (!has_isomorphic_node) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-// Returns true if TreeNode n2 has a subtree which is isomorphic to n1.
-bool isSubgraphIsomorphic(const TreeNode &n1, const TreeNode &n2) {
-  std::queue<const TreeNode *> candidates;
-  candidates.push(&n2);
-
-  while (candidates.size() > 0) {
-    const TreeNode *node = candidates.front();
-    if (isIsomorphic(n1, *node)) {
-      return true;
-    }
-
-    for (const auto &child : node->children()) {
-      candidates.push(&child);
-    }
-    candidates.pop();
-  }
-
-  return false;
-}
-
-TEST(GraphProtoTest, SingleTreeNode) {
-  TreeNode n1;
-  n1.set_id("n1");
-  TreeNode n2;
-  n2.set_id("n2");
-  EXPECT_TRUE(isIsomorphic(n1, n2));
-  n2.set_id("n1");
-  EXPECT_TRUE(isIsomorphic(n1, n2));
-}
-
-TEST(GraphProtoTest, TreeNodeMatches) {
-  TreeNode n1;
-  n1.set_id("n1");
-  n1.mutable_properties()->insert({"a", "x"});
-  TreeNode n2;
-  n2.set_id("n2");
-  n2.mutable_properties()->insert({"a", "x"});
-  n2.mutable_properties()->insert({"b", "y"});
-  EXPECT_TRUE(isIsomorphic(n1, n2));
-  n2.clear_properties();
-  EXPECT_FALSE(isIsomorphic(n1, n2));
-}
-
-TEST(GraphProtoTest, ChildMatch) {
-  TreeNode n1;
-  n1.mutable_properties()->insert({"a", "x"});
-  TreeNode *n1_child = n1.mutable_children()->Add();
-  n1_child->mutable_properties()->insert({"b", "y"});
-  TreeNode n2;
-  n2.mutable_properties()->insert({"a", "x"});
-  TreeNode *n2_child = n2.mutable_children()->Add();
-  n2_child->mutable_properties()->insert({"b", "y"});
-  n2_child = n2.mutable_children()->Add();
-  n2_child->mutable_properties()->insert({"c", "z"});
-  EXPECT_TRUE(isIsomorphic(n1, n2));
-}
 
 TEST(FilterTest, MapUpdate) {
   std::map<std::string, std::string> spans_to_headers;
@@ -142,7 +49,7 @@ TEST(FilterTest, SplitStr) {
   EXPECT_EQ(result, "x-a-b,x-a-c,x-a-d");
 }
 
-TEST(FilterTest, SplitStrTreeNodelim) {
+TEST(FilterTest, SplitStrNoDelim) {
   std::string s{"a-b"};
   std::regex delimiter(",");
   std::sregex_token_iterator it{s.begin(), s.end(), delimiter, -1};
