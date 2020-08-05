b75e3d1ac360a9186433dfa72cde50b0f60bda93
Taegyun Kim <k.taegyun@gmail.com>
2020-08-05 17:33:21 -0400

    lets just hardcode what we need for getting properties

--- wasm/filter.cc
+++ wasm/filter.cc
@@ -7,7 +7,6 @@
 #include <unordered_map>
 
 #include "proxy_wasm_intrinsics.h"
-#include "treenode.pb.h"
 
 // TrafficDirection is a mirror of envoy xDS traffic direction.
 // As defined in istio/proxy/extensions/common/context.h
@@ -36,18 +35,6 @@ std::string trafficDirectionToString(TrafficDirection dir) {
   }
 }
 
-static unsigned char PROTO_BYTES[130] = {
-    10,  1,   97,  18,  29,  10,  12,  115, 101, 114, 118, 105, 99,  101, 95,
-    110, 97,  109, 101, 18,  13,  112, 114, 111, 100, 117, 99,  116, 112, 97,
-    103, 101, 118, 49,  26,  62,  10,  1,   98,  18,  25,  10,  12,  115, 101,
-    114, 118, 105, 99,  101, 95,  110, 97,  109, 101, 18,  9,   114, 101, 118,
-    105, 101, 119, 115, 118, 50,  26,  30,  10,  1,   99,  18,  25,  10,  12,
-    115, 101, 114, 118, 105, 99,  101, 95,  110, 97,  109, 101, 18,  9,   114,
-    97,  116, 105, 110, 103, 115, 118, 49,  26,  30,  10,  1,   100, 18,  25,
-    10,  12,  115, 101, 114, 118, 105, 99,  101, 95,  110, 97,  109, 101, 18,
-    9,   100, 101, 116, 97,  105, 108, 115, 118, 49,
-};
-
 class BidiRootContext : public RootContext {
 public:
   explicit BidiRootContext(uint32_t id, StringView root_id)
@@ -56,26 +43,16 @@ public:
     if (getValue({"node", "metadata", "WORKLOAD_NAME"}, &workload_name)) {
       workload_name_ = workload_name;
       LOG_WARN("Intialized workload_name: " + workload_name_);
-
-      if (workload_name_ == "productpagev1") {
-        query_ = std::make_unique<TreeNode>();
-        query_->ParseFromArray(&PROTO_BYTES, 130);
-      }
     } else {
       LOG_WARN("Failed to set workload name");
     }
-
-    properties_to_collect_ = {{"node", "metadata", "WORKLOAD_NAME"}};
   }
   bool onConfigure(size_t /* configuration_size */) override;
 
   StringView getWorkloadName() { return workload_name_; }
 
-  std::vector<std::initializer_list<StringView>> properties_to_collect_;
-
 private:
   std::string workload_name_;
-  std::unique_ptr<TreeNode> query_;
 };
 
 class BidiContext : public Context {
@@ -180,12 +157,6 @@ FilterHeadersStatus BidiContext::onResponseHeaders(uint32_t) {
       }
       std::set<std::string> words_set{words.begin(), words.end()};
 
-      if (workload_name == "productpagev1") {
-        // TODO: Construct TreeNode graph using paths and properties returned
-        // and check whether the query is subgraph isomorphic to the graph
-        // generated from request trace.
-      }
-
       // Now join them.
       std::string joined = std::accumulate(
           words.begin(), words.end(), std::string(),
@@ -194,32 +165,45 @@ FilterHeadersStatus BidiContext::onResponseHeaders(uint32_t) {
           });
       addResponseHeader("x-wasm", joined);
       LOG_WARN("inbound: x-wasm -> " + joined);
+
+      if (workload_name == "productpagev1") {
+        // TODO: Construct TreeNode graph using paths and properties returned
+        // and check whether the query is subgraph isomorphic to the graph
+        // generated from request trace.
+      }
+
     } else {
       addResponseHeader("x-wasm", std::string(workload_name));
       LOG_WARN("inbound: x-wasm -> " + std::string(workload_name));
     }
 
+    logWarn("inbound: proceed to collect properties.");
     // Collect properties
     std::vector<std::string> properties;
-    for (const auto &property : root_->properties_to_collect_) {
-      std::string value;
-      if (getValue(property, &value)) {
-        std::string result = std::string(root_->getWorkloadName());
-        for (const auto &p : property) {
-          result += "." + std::string(p);
-        }
-        result += "==";
-        result += value;
-
-        properties.push_back(result);
+    std::string value;
+    if (getValue({"node", "metadata", "WORKLOAD_NAME"}, &value)) {
+      std::string result = std::string(root_->getWorkloadName());
+      for (auto p : {"node", "metadata", "WORKLOAD_NAME"}) {
+        result += "." + std::string(p);
       }
+      result += "==";
+      result += value;
+
+      properties.push_back(result);
+    } else {
+      logWarn("failed to get property");
     }
+
+    logWarn("inbound: number of properties collected " +
+            std::to_string(properties.size()));
 
     std::string properties_joined = std::accumulate(
         properties.begin(), properties.end(), std::string(),
         [](const std::string &a, const std::string &b) -> std::string {
           return a + (a.length() > 0 ? "," : "") + b;
         });
+
+    logWarn("inbound: properties_joined:" + properties_joined);
 
     WasmDataPtr property_shared_data;
     result = getSharedData(b3_span_id_ + "property", &property_shared_data);
@@ -237,6 +221,9 @@ FilterHeadersStatus BidiContext::onResponseHeaders(uint32_t) {
       logWarn("inbound: x-wasm -> " + properties_joined);
     }
 
+    if (workload_name == "productpagev1") {
+      logWarn("Collected properties: " + properties_joined);
+    }
   } else if (direction_ == TrafficDirection::Outbound) {
     // Received response from another service we called.
     // Collect x-wasm header value.
@@ -265,12 +252,14 @@ FilterHeadersStatus BidiContext::onResponseHeaders(uint32_t) {
           getSharedData(b3_parent_span_id_ + "property", &shared_data);
       if (result == WasmResult::Ok && shared_data->data() != nullptr) {
         LOG_WARN("outbound: x-wasm -> " + shared_data->toString() + "," +
-                 header->toString());
+                 property_header->toString());
         setSharedData(b3_parent_span_id_ + "property",
-                      shared_data->toString() + "," + header->toString());
+                      shared_data->toString() + "," +
+                          property_header->toString());
       } else {
         LOG_WARN("outbound: x-wasm -> " + header->toString());
-        setSharedData(b3_parent_span_id_ + "property", header->toString());
+        setSharedData(b3_parent_span_id_ + "property",
+                      property_header->toString());
       }
     }
   }
