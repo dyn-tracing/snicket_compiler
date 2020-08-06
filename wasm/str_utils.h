6eaf4ae9768f670b60f0af969551bf8903e4b050
Taegyun Kim <k.taegyun@gmail.com>
2020-08-05 23:48:52 -0400

    Copy str_utils

---
+++ wasm/str_utils.h
@@ -0,0 +1,14 @@
+#include <initializer_list>
+#include <string>
+#include <string_view>
+#include <vector>
+
+std::vector<std::string> str_split(const std::string &str,
+                                   const std::string &delim,
+                                   bool filter_empty = false);
+
+std::string str_join(std::initializer_list<std::string_view> strs,
+                     std::string_view delim);
+
+std::string str_join(std::vector<std::string_view> strs,
+                     std::string_view delim);
