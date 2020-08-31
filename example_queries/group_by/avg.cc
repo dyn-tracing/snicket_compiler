c2c6a3bda03c52e48f308c1df09b70356166490d
Taegyun Kim <k.taegyun@gmail.com>
2020-08-31 16:21:52 -0400

    moving average

---
+++ example_queries/group_by/avg.cc
@@ -0,0 +1,14 @@
+// udf_type: Aggregation
+// id: avg
+// return_type: float
+
+class avg {
+public:
+  float operator()(int value) {
+    avg_  = avg_ + ((float)value - avg_ ) / (count_ + 1);
+    count_ += 1;
+  }
+
+  int count_ =  0;
+  float avg_ = 0.0;
+};
