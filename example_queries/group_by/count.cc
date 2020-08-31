54bee942893f23c9b2c4ce504c0f2c37f660457a
Taegyun Kim <k.taegyun@gmail.com>
2020-08-31 14:02:48 -0400

    remove arg from udfs

---
+++ example_queries/group_by/count.cc
@@ -0,0 +1,14 @@
+// udf_type: Aggregation
+// id: count
+// return_type: int
+
+class count {
+public:
+  int operator()(int height) {
+    buckets_[height] += 1;
+
+    return buckets_[height];
+  }
+
+  std::map<int, int> buckets_;
+};
