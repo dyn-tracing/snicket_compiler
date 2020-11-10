cef411067dfdfa7b4e0a17bbd6f933a0eea30e56
fruffy <fruffy@nyu.edu>
2020-11-10 18:11:20 +0100

    Some more notes.

--- deps/commands.sh
+++ deps/commands.sh
@@ -1,20 +1,33 @@
+# WARNING: This is not a script that should be run yet
+# These are useful commands for setting up and testing a bookinfo cluster
+
 minikube start
 #bin/istioctl manifest apply --set profile=demo
 # or for newer versions
-bin/istioctl install --set profile=demo
+istio-1.7.4/bin/istioctl install --set profile=demo
 kubectl label namespace default istio-injection=enabled
-# these are the "patched" applications
-kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
-kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
-kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
-kubectl apply -f samples/bookinfo/networking/destination-rule-all-mtls.yaml
-./bin/istioctl analyze
+# these are the istio applications
+kubectl apply -f istio-1.7.4/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f istio-1.7.4/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f istio-1.7.4/samples/bookinfo/networking/destination-rule-all.yaml
+kubectl apply -f istio-1.7.4/samples/bookinfo/networking/destination-rule-all-mtls.yaml
+kubectl apply -f istio-1.7.4/samples/httpbin/sample-client/fortio-deploy.yaml
+istio-1.7.4/bin/istioctl analyze
 export INGRESS_PORT=(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 export INGRESS_HOST=(minikube ip)
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 echo http://"$GATEWAY_URL/productpage"
+
+# Fortio
+export FORTIO_POD=(kubectl get pods -lapp=fortio -o 'jsonpath={.items[0].metadata.name}')
+# execute a single request with fortio
+kubectl exec "$FORTIO_POD" -c fortio -- /usr/bin/fortio curl -quiet http://"$GATEWAY_URL/productpage"
+# execute 20 requests with fortio
+kubectl exec "$FORTIO_POD" -c fortio -- /usr/bin/fortio load -c 2 -qps 0 -n 20 -loglevel Warning http://"$GATEWAY_URL/productpage"
+
 # run on separate shell
-minikube tunnelkubectl apply -f wasm/storage_upstream.yaml
+minikube tunnel
+kubectl apply -f wasm/storage_upstream.yaml
 kubectl apply -f wasm/productpage-cluster.yaml
 target/debug/dyntracing -q example_queries/return.cql
 git clone -b patch-context git@github.com:taegyunkim/wasme.git
@@ -31,6 +44,15 @@ bazel build :filter.wasm
 ../wasme/_output/wasme push webassemblyhub.io/fruffy/testfilter:1
 ../wasme/_output/wasme deploy istio webassemblyhub.io/fruffy/testfilter:1 --id=1 --namespace=default
 kubectl exec -it (kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl -v storage-upstream:8080/list# deletion
-samples/bookinfo/platform/kube/cleanup.sh
+
+istio-1.7.4/samples/bookinfo/platform/kube/cleanup.sh
 kubectl delete service/storage-upstream
 kubectl delete deployment.apps/storage-upstream
+
+
+# deploy Grafana and Prometheus
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.7/samples/addons/kiali.yaml
+# access them
+./istio-1.7.4/bin/istioctl dashboard grafana
