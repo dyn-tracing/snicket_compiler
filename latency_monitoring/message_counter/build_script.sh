34ecb2dcd502441ff4af32c1513c22185a0f92d1
fruffy <fruffy@nyu.edu>
2020-11-30 17:00:06 +0100

    Fixes.

--- latency_monitoring/message_counter/build_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bazel build //:filter.wasm
-#~/.wasme/bin/wasme build precompiled ./bazel-bin/filter.wasm --tag webassemblyhub.io/jb7399/message_counter:1  --config runtime-config.json
-# wasme build cpp -t test-filter . --tag webassemblyhub.io/fruffy/test-filter:1 --config runtime-config.json
-# wasme push webassemblyhub.io/fruffy/test-filter:1
-
-
