096655e2962b67f7a7879bbd63656620f5ceaebb
fruffy <fruffy@nyu.edu>
2020-12-02 11:00:55 +0100

    Fix the filter.

--- latency_monitoring/message_counter/filter.cc
+++ latency_monitoring/message_counter/filter.cc
@@ -1,10 +1,10 @@
-// NOLINT(namespace-envoy)
+#include <set>
 #include <string>
 #include <unordered_map>
 
+#include "filter.pb.h"
 #include "google/protobuf/util/json_util.h"
 #include "proxy_wasm_intrinsics.h"
-#include "filter.pb.h"
 
 #define REQUEST_THRESHOLD 10
 #define STORAGE_NAME "storage-upstream"
@@ -34,11 +34,12 @@ std::string trafficDirectionToString(TrafficDirection dir) {
     }
 }
 
-class AddHeaderRootContext : public RootContext {
-public:
-  explicit AddHeaderRootContext(uint32_t id, std::string_view root_id) : RootContext(id, root_id) {
-    std::string workload_name;
-    if (getValue({"node", "metadata", "WORKLOAD_NAME"}, &workload_name)) {
+class BidiRootContext : public RootContext {
+ public:
+    explicit BidiRootContext(uint32_t id, std::string_view root_id)
+        : RootContext(id, root_id) {
+        std::string workload_name;
+        if (getValue({"node", "metadata", "WORKLOAD_NAME"}, &workload_name)) {
             workload_name_ = workload_name;
             std::string warning = "initialized workload_name: ";
             warning = warning.append(workload_name_);
@@ -46,44 +47,115 @@ public:
         } else {
             LOG_WARN("Failed to set workload name");
         }
-        count_ = 0;
-  }
-  bool onConfigure(size_t /* configuration_size */) override;
+        int tmp_count = 0;
+        std::string count_key = "count";
+        proxy_set_shared_data(count_key.c_str(), count_key.size(),
+                              (const char *)&tmp_count, sizeof(tmp_count), 0);
+    }
+    bool onConfigure(size_t /* configuration_size */) override;
 
-  bool onStart(size_t) override;
-  std::string getWorkloadName() { return workload_name_; }
-  void incrementCount() { count_++; }
-  void decrementCount() { count_--; }
-  int getCount() { return count_; }
-  std::string header_value_;
+    bool onStart(size_t) override;
+    std::string getWorkloadName() { return workload_name_; }
+    void incrementCount() {
+        int *tmp_count;
+        size_t *size;
+        WasmResult result;
 
-private:
-  std::string workload_name_;
-  int count_;
+        std::string count_key = "count";
+        result = proxy_get_shared_data(count_key.c_str(), count_key.size(),
+                              (const char **)&tmp_count, size, 0);
+        if (result != WasmResult::Ok) {
+            LOG_ERROR("Failed to get shared data " + count_key + ".");
+            return;
+        }
+        (*tmp_count)++;
+        result = proxy_set_shared_data(count_key.c_str(), count_key.size(),
+                              (const char *)tmp_count, sizeof(*tmp_count), 0);
+        if (result != WasmResult::Ok) {
+            LOG_ERROR("Failed to set shared data " + count_key + ".");
+        }
+    }
+    void decrementCount() {
+        WasmResult result;
+        int *tmp_count;
+        size_t *size;
+        std::string count_key = "count";
+        result = proxy_get_shared_data(count_key.c_str(), count_key.size(),
+                              (const char **)&tmp_count, size, 0);
+        if (result != WasmResult::Ok) {
+            LOG_ERROR("Failed to get shared data " + count_key + ".");
+            return;
+        }
+        (*tmp_count)--;
+        result = proxy_set_shared_data(count_key.c_str(), count_key.size(),
+                              (const char *)tmp_count, sizeof(*tmp_count), 0);
+        if (result != WasmResult::Ok) {
+            LOG_ERROR("Failed to set shared data " + count_key + ".");
+        }
+    }
+    int getCount() {
+        WasmResult result;
+        int *tmp_count;
+        size_t *size;
+        std::string count_key = "count";
+        result = proxy_get_shared_data(count_key.c_str(), count_key.size(),
+                              (const char **)&tmp_count, size, 0);
+        if (result != WasmResult::Ok) {
+            LOG_ERROR("Failed to get shared data " + count_key + ".");
+            return -1;
+        }
+        return *tmp_count;
+    }
+    std::string header_value_;
+    std::set<std::string> pending_requests;
+
+ private:
+    std::string workload_name_;
 };
 
-class AddHeaderContext : public Context {
-public:
-  explicit AddHeaderContext(uint32_t id, RootContext* root) : Context(id, root), root_(static_cast<AddHeaderRootContext*>(static_cast<void*>(root))) {}
+class BidiContext : public Context {
+ public:
+    explicit BidiContext(uint32_t id, RootContext *root)
+        : Context(id, root),
+          root_(static_cast<BidiRootContext *>(static_cast<void *>(root))) {
+        direction_ = getTrafficDirection();
 
-  void onCreate() override;
-  FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
-  FilterDataStatus onRequestBody(size_t body_buffer_length, bool end_of_stream) override;
-  FilterHeadersStatus onResponseHeaders(uint32_t headers, bool end_of_stream) override;
-  void onDone() override;
-  void onLog() override;
-  void onDelete() override;
-private:
-  WasmResult store_warning();
-  AddHeaderRootContext* root_;
+        gauge_ = Gauge<int>::New("queue_gauge", "queue_size");
+    }
+
+    void onCreate() override;
+    FilterHeadersStatus onRequestHeaders(uint32_t headers,
+                                         bool end_of_stream) override;
+    FilterHeadersStatus onRequestHeadersInbound();
+    FilterHeadersStatus onRequestHeadersOutbound();
+    FilterDataStatus onRequestBody(size_t body_buffer_length,
+                                   bool end_of_stream) override;
+    FilterDataStatus onResponseBody(size_t body_buffer_length,
+                                    bool end_of_stream) override;
+    FilterHeadersStatus onResponseHeaders(uint32_t headers,
+                                          bool end_of_stream) override;
+    FilterHeadersStatus onResponseHeadersInbound();
+    FilterHeadersStatus onResponseHeadersOutbound();
+
+    void onDone() override;
+    void onLog() override;
+    void onDelete() override;
+
+ private:
+    BidiRootContext *root_;
+    TrafficDirection direction_;
+    Gauge<int> *gauge_;
+    WasmResult store_warning();
+    void print_headers(WasmHeaderMapType type);
 };
-static RegisterContextFactory register_AddHeaderContext(CONTEXT_FACTORY(AddHeaderContext),
-                                                      ROOT_FACTORY(AddHeaderRootContext),
-                                                      "add_header_root_id");
 
-WasmResult AddHeaderContext::store_warning(std::string count) {
+static RegisterContextFactory
+    register_BidiContext(CONTEXT_FACTORY(BidiContext),
+                         ROOT_FACTORY(BidiRootContext), "bidi_root_id");
+
+WasmResult BidiContext::store_warning() {
     std::string key = toString(getCurrentTimeNanoseconds());
-    std::string value = root_->getWorkloadName() + ":" + count;
+    std::string value = root_->getWorkloadName();
     LOG_WARN("Storing timestamp " + key + " as node " + value + ".");
     auto context_id = id();
     auto callback = [context_id](uint32_t, size_t body_size, uint32_t) {
@@ -106,48 +178,169 @@ WasmResult AddHeaderContext::store_warning(std::string count) {
     return result;
 }
 
-bool AddHeaderRootContext::onConfigure(size_t config_buffer_length) {
-  auto conf = getBufferBytes(WasmBufferType::PluginConfiguration, 0, config_buffer_length);
-  LOG_DEBUG("onConfigure " + conf->toString());
-  header_value_ = conf->toString();
-  return true; 
+void BidiContext::print_headers(WasmHeaderMapType type) {
+    if (type == WasmHeaderMapType::RequestHeaders) {
+        auto result = getRequestHeaderPairs();
+        auto pairs = result->pairs();
+        LOG_WARN("Request headers: " + toString(pairs.size()));
+        for (auto &p : pairs) {
+            LOG_WARN(std::string(p.first) + " -> " + std::string(p.second));
+        }
+    } else if (type == WasmHeaderMapType::ResponseHeaders) {
+        auto result = getResponseHeaderPairs();
+        auto pairs = result->pairs();
+        LOG_WARN("Response headers: " + toString(pairs.size()));
+        for (auto &p : pairs) {
+            LOG_WARN(std::string(p.first) + " -> " + std::string(p.second));
+        }
+    }
 }
 
-bool AddHeaderRootContext::onStart(size_t) { LOG_DEBUG("onStart"); return true;}
+////////////////// REQUEST HEADERS //////////////////
 
-void AddHeaderContext::onCreate() { LOG_DEBUG(std::string("onCreate " + std::to_string(id()))); }
+FilterHeadersStatus BidiContext::onRequestHeaders(uint32_t, bool) {
+    FilterHeadersStatus status;
 
-FilterHeadersStatus AddHeaderContext::onRequestHeaders(uint32_t, bool) {
-  LOG_DEBUG(std::string("onRequestHeaders ") + std::to_string(id()));
-  LOG_WARN(std::string("onRequestHeaders ") + std::to_string(id()));
-  root_->incrementCount();
-  std::string count_as_string = std::to_string(root_->getCount());
-  LOG_WARN("count is " + count_as_string);
-  LOG_WARN("workload name is " + root_->getWorkloadName());
-  store_warning(count_as_string);
-  return FilterHeadersStatus::Continue;
+    LOG_WARN("REQUEST BEGIN ############################################");
+    // Print all request headers
+    print_headers(WasmHeaderMapType::RequestHeaders);
+
+    replaceRequestHeader("x-envoy-force-trace", "true");
+    if (direction_ == TrafficDirection::Inbound) {
+        status = onRequestHeadersInbound();
+    } else if (direction_ == TrafficDirection::Outbound) {
+        status = onRequestHeadersOutbound();
+    } else {
+        LOG_ERROR("Missing request header direction.");
+        status = FilterHeadersStatus::Continue;
+    }
+    LOG_WARN("REQUEST END ############################################");
+    return status;
 }
 
-FilterHeadersStatus AddHeaderContext::onResponseHeaders(uint32_t, bool) {
-  LOG_DEBUG(std::string("onResponseHeaders ") + std::to_string(id()));
-  LOG_WARN(std::string("onResponseHeaders ") + std::to_string(id()));
-  //addResponseHeader("newheader", root_->header_value_);
-  addResponseHeader("hi", "hello");
-  root_->decrementCount();
-  std::string count_as_string = std::to_string(root_->getCount());
-  LOG_WARN("count is " + count_as_string);
-  LOG_WARN("workload name is " + root_->getWorkloadName());
-  replaceResponseHeader("location", "envoy-wasm");
-  store_warning(count_as_string);
-  return FilterHeadersStatus::Continue;
+FilterHeadersStatus BidiContext::onRequestHeadersInbound() {
+    LOG_WARN("Inbound request.");
+    return FilterHeadersStatus::Continue;
 }
 
-FilterDataStatus AddHeaderContext::onRequestBody(size_t body_buffer_length, bool end_of_stream) {
-  return FilterDataStatus::Continue;
+FilterHeadersStatus BidiContext::onRequestHeadersOutbound() {
+    LOG_WARN("Outbound request.");
+    auto request_id = getRequestHeader("x-request-id");
+    if (request_id->data() == nullptr) {
+        LOG_WARN(trafficDirectionToString(direction_) + " " +
+                 "x-request-id not found!");
+        return FilterHeadersStatus::Continue;
+    }
+    // std::string x_request_id_ = request_id->toString();
+    // LOG_WARN("Inserting pending request " + x_request_id_ + ".");
+    // root_->pending_requests.insert(x_request_id_);
+    // auto curr_queue_size = root_->pending_requests.size();
+    // gauge_->record(curr_queue_size, 0);
+    root_->incrementCount();
+    auto curr_queue_size = root_->getCount();
+    LOG_WARN("Current size of request queue: " + toString(curr_queue_size));
+    if (curr_queue_size > REQUEST_THRESHOLD) {
+        LOG_ERROR("Request queue " + toString(curr_queue_size) +
+                  " is above threshold " + toString(REQUEST_THRESHOLD) + ".");
+        store_warning();
+    }
+    return FilterHeadersStatus::Continue;
 }
 
-void AddHeaderContext::onDone() { LOG_DEBUG(std::string("onDone " + std::to_string(id()))); }
+////////////////// RESPONSE HEADERS //////////////////
 
-void AddHeaderContext::onLog() { LOG_DEBUG(std::string("onLog " + std::to_string(id()))); }
+FilterHeadersStatus BidiContext::onResponseHeaders(uint32_t, bool) {
+    FilterHeadersStatus status;
 
-void AddHeaderContext::onDelete() { LOG_DEBUG(std::string("onDelete " + std::to_string(id()))); }
+    LOG_WARN("RESPONSE BEGIN ############################################");
+    print_headers(WasmHeaderMapType::ResponseHeaders);
+    replaceResponseHeader("location", "envoy-wasm");
+    replaceResponseHeader("x-envoy-force-trace", "true");
+    if (direction_ == TrafficDirection::Inbound) {
+        status = onResponseHeadersInbound();
+    } else if (direction_ == TrafficDirection::Outbound) {
+        status = onResponseHeadersOutbound();
+    } else {
+        LOG_ERROR("Missing response header direction.");
+        status = FilterHeadersStatus::Continue;
+    }
+    LOG_WARN("RESPONSE END ############################################");
+    return status;
+}
+
+FilterHeadersStatus BidiContext::onResponseHeadersInbound() {
+    LOG_WARN("Inbound response.");
+    return FilterHeadersStatus::Continue;
+}
+
+FilterHeadersStatus BidiContext::onResponseHeadersOutbound() {
+    LOG_WARN("Outbound response.");
+    auto request_id = getResponseHeader("x-request-id");
+    if (request_id->data() == nullptr) {
+        LOG_WARN(trafficDirectionToString(direction_) + " " +
+                 "x-request-id not found!");
+        return FilterHeadersStatus::Continue;
+    }
+    // std::string x_request_id_ = request_id->toString();
+    // root_->pending_requests.erase(x_request_id_);
+    // auto curr_queue_size = root_->pending_requests.size();
+    // LOG_WARN("Removing pending request " + x_request_id_ + ".");
+    root_->decrementCount();
+    auto curr_queue_size = root_->getCount();
+    gauge_->record(curr_queue_size, 0);
+    LOG_WARN("Current size of request queue: " + toString(curr_queue_size));
+    return FilterHeadersStatus::Continue;
+}
+
+////////////////// REQUEST BODY //////////////////
+
+FilterDataStatus BidiContext::onRequestBody(size_t body_buffer_length,
+                                            bool end_of_stream) {
+    return FilterDataStatus::Continue;
+}
+
+////////////////// RESPONSE BODY //////////////////
+
+FilterDataStatus BidiContext::onResponseBody(size_t body_buffer_length,
+                                             bool end_of_stream) {
+    return FilterDataStatus::Continue;
+}
+
+////////////////// CONFIGURE //////////////////
+
+bool BidiRootContext::onConfigure(size_t config_buffer_length) {
+    auto conf = getBufferBytes(WasmBufferType::PluginConfiguration, 0,
+                               config_buffer_length);
+    LOG_DEBUG("onConfigure " + conf->toString());
+    header_value_ = conf->toString();
+    return true;
+}
+
+////////////////// START //////////////////
+
+bool BidiRootContext::onStart(size_t) {
+    LOG_DEBUG("onStart");
+    return true;
+}
+
+////////////////// CREATE //////////////////
+
+void BidiContext::onCreate() {
+    LOG_DEBUG(std::string("onCreate " + toString(id())));
+}
+
+////////////////// DONE //////////////////
+
+void BidiContext::onDone() {
+    LOG_DEBUG(std::string("onDone " + toString(id())));
+}
+
+////////////////// LOG //////////////////
+
+void BidiContext::onLog() { LOG_DEBUG(std::string("onLog " + toString(id()))); }
+
+////////////////// DELETE //////////////////
+
+void BidiContext::onDelete() {
+    LOG_DEBUG(std::string("onDelete " + toString(id())));
+}
