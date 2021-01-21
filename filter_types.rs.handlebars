5d8d350b02c1c61c89a21f1c4db6585163a927ae
Jessica Berg <jb7399@nyu.edu>
2021-01-21 15:51:19 -0500

    cargo fmt and cargo clippy changes

--- filter_types.rs.handlebars
+++ filter_types.rs.handlebars
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::fs;
 use crate::rpc::Rpc;
 
 pub type CodeletType = fn(&Filter, &Rpc) -> Option<Rpc>;
--- src/main.rs
+++ src/main.rs
@@ -128,17 +128,12 @@ fn main() {
     // Use the information in the code generator code_gen and format it, using handlebars
     // to a template with all the basic filter information enclosed
     let c_mode = matches.value_of("compilation_mode").unwrap();
-    let mut output_name = PathBuf::from(matches.value_of("output").unwrap());
+    let output_name = PathBuf::from(matches.value_of("output").unwrap());
     if c_mode == "sim" {
         // Because we are making a library, not just one file, we need to copy over an example library.  Then,
         // we have to write to three files:  Cargo.toml to edit the filter name, lib.rs to edit the filter name,
         // and <output_name>.rs for the actual filter implementation
-        let src = "src";
         let rust_dir = bin_dir.join("rust_filter");
-
-        // Making new library folder
-        let lib_src_folder = output_name.join(src);
-        fs::create_dir_all(&lib_src_folder).unwrap();
 
         // Filter types
         let filter_types_file = rust_dir.join("src").join("types.rs");
@@ -151,7 +146,7 @@ fn main() {
 
         // The filter itself
         let mut filter_file_name = rust_dir.join("src").join(output_name);
-        filter_file_name.set_extension(".rs");
+        filter_file_name.set_extension("rs");
         let filter_name_handlebars = bin_dir.join("filter.rs.handlebars");
         generate_code_from_codegen_with_handlebars(
             &code_gen,
@@ -160,7 +155,6 @@ fn main() {
         );
     } else {
         let filter_handlebars_cc = bin_dir.join("filter.cc.handlebars");
-        output_name.set_extension("cc");
         generate_code_from_codegen_with_handlebars(&code_gen, filter_handlebars_cc, output_name);
     }
 }
