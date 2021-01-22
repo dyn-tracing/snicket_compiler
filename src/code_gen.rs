5fbcd2d7fccb33c9f5f7e02fa14950702e029aad
Jessica Berg <jb7399@nyu.edu>
2021-01-22 17:08:06 -0500

    Breadth implemented for root

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -398,11 +398,17 @@ std::string {cpp_var_id} = node_ptr->properties.at({parts});",
                     );
 
                     self.cpp_blocks.push(cpp_block);
-                    let rust_block = format!(
-                        "let {cpp_var_id} = get_out_degree(target, mapping->at(\"{node_id}\")).to_string());",
-                        cpp_var_id = cpp_var_id,
-                        node_id = id.id_name,
-                    );
+
+                    let rust_index_code = format!("let node_index = graph_utils::get_node_with_id(&target, \"{node_id}\".to_string());\n                if node_index.is_none() {{\n                    print!(\"WARNING: could not find node with id\");\n                }}\n",
+                            node_id = id.id_name);
+                    let rust_get_breadth_code = format!(
+                            "               else {{\n                    let trace_index = NodeIndex::new(mapping[node_index.unwrap().index()]);\n                    let {cpp_var_id} = graph_utils::get_out_degree(&trace_graph, Some(trace_index)); // we add one for ourselves - the node we are on is not added to the path until after the filter is run\n",
+                            cpp_var_id = cpp_var_id,
+                        );
+                    let rust_error_check = format!("                    let ret = fs::write(\"result.txt\", {cpp_var_id}.to_string());\n                    match ret {{\n                        Ok(result) => result,\n                        Err(_e) => print!(\"WARNING: could not write result to file\"),\n                    }}; \n                }}",
+                            cpp_var_id = cpp_var_id,
+                        );
+                    let rust_block = format!("{}{}{}", rust_index_code, rust_get_breadth_code, rust_error_check);
 
                     self.rust_blocks.push(rust_block);
                     self.result = CppResult::Return {
