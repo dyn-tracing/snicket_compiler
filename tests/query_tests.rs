d9846e9df8e68e33bebacaabf4ec6a6dcffb3ead
fruffy <fruffy@nyu.edu>
2021-02-18 17:24:34 -0500

    Maintain references files.

--- tests/query_tests.rs
+++ tests/query_tests.rs
@@ -1,4 +1,6 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use diffy;
+use std::fs;
 use std::path::Path; // Directory management
 use std::process::Command; // Run programs
 use test_case::test_case; // Parametrized tests
@@ -25,24 +27,38 @@ fn check_compilation(
     // This is the binary to compile a query
     let mut cmd = Command::new(proj_dir.join("target/debug/dtc"));
     // Assemble the args, first the input query
-    let mut args = vec!["-q".to_string(), query_file.to_str().unwrap().into()];
+    let mut args = vec!["-q", query_file.to_str().unwrap()];
     // Append every udf that is provided
+    let mut str_builder = String::new();
     for udf_name in udf_names {
         let udf_file = udf_dir.join(udf_name);
-        args.push("-u".to_string());
-        args.push(udf_file.to_str().unwrap().into());
+        str_builder.push_str("-u");
+        str_builder.push_str(udf_file.to_str().unwrap());
+    }
+    if !str_builder.is_empty() {
+        args.push(&str_builder);
     }
     let mut out_file = query_dir.join(query_name);
     out_file.set_extension("cc");
-    args.extend(vec!["-o".to_string(), out_file.to_str().unwrap().into()]);
-    args.extend(vec![
-        "--root-node".to_string(),
-        "productpage-v1".to_string(),
-    ]);
+    args.extend(vec!["-o", out_file.to_str().unwrap()]);
+    args.extend(vec!["--root-node", "productpage-v1"]);
     cmd.args(args);
     cmd.assert().success();
 
-    Ok(())
+    let mut ref_file = query_dir.join(query_name);
+    ref_file.set_extension("cc.ref");
+    let out_file_str = fs::read_to_string(out_file).unwrap();
+    let ref_file_str = fs::read_to_string(ref_file).unwrap();
+    if out_file_str != ref_file_str {
+        let diff = diffy::create_patch(&out_file_str, &ref_file_str);
+        let diff_color = diffy::PatchFormatter::new().with_color();
+        panic!(
+            "Files differ in the following way:\n{}",
+            diff_color.fmt_patch(&diff)
+        );
+    } else {
+        Ok(())
+    }
 }
 
 #[test_case("breadth_histogram.cql", vec!["histogram.rs"];
@@ -70,19 +86,37 @@ fn check_compilation_rust(
     // This is the binary to compile a query
     let mut cmd = Command::new(proj_dir.join("target/debug/dtc"));
     // Assemble the args, first the input query
-    let mut args = vec!["-q".to_string(), query_file.to_str().unwrap().into()];
+    let mut args = vec!["-q", query_file.to_str().unwrap()];
     // Append every udf that is provided
+    let mut str_builder = String::new();
     for udf_name in udf_names {
         let udf_file = udf_dir.join(udf_name);
-        args.push("-u".to_string());
-        args.push(udf_file.to_str().unwrap().into());
+        str_builder.push_str("-u");
+        str_builder.push_str(udf_file.to_str().unwrap());
     }
-    let out_file = String::from("filter");
-    args.extend(vec!["-o".to_string(), out_file]);
-    args.extend(vec!["-c".to_string(), String::from("sim")]);
-    args.extend(vec!["--root-node".to_string(), "0".to_string()]);
+    if !str_builder.is_empty() {
+        args.push(&str_builder);
+    }
+    let mut out_file = query_dir.join(query_name);
+    out_file.set_extension("rs");
+    args.extend(vec!["-o", out_file.to_str().unwrap()]);
+    args.extend(vec!["-c", "sim"]);
+    args.extend(vec!["--root-node", "0"]);
     cmd.args(args);
     cmd.assert().success();
 
-    Ok(())
+    let mut ref_file = query_dir.join(query_name);
+    ref_file.set_extension("rs.ref");
+    let out_file_str = fs::read_to_string(out_file).unwrap();
+    let ref_file_str = fs::read_to_string(ref_file).unwrap();
+    if out_file_str != ref_file_str {
+        let diff = diffy::create_patch(&out_file_str, &ref_file_str);
+        let diff_color = diffy::PatchFormatter::new().with_color();
+        panic!(
+            "Files differ in the following way:\n{}",
+            diff_color.fmt_patch(&diff)
+        );
+    } else {
+        Ok(())
+    }
 }
