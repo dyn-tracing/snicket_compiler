fa6f6a31bda1661e3503b50a28dce4cf7807cfbb
fruffy <fruffy@nyu.edu>
2021-03-10 12:58:03 -0500

    Quick fix.

--- cpp_filter/str_utils.cc
+++ cpp_filter/str_utils.cc
@@ -6,35 +6,54 @@
 
 #include "str_utils.h"
 
-// trim from start
-static inline std::string &ltrim(std::string &s) {
-    s.erase(s.begin(),
-            std::find_if(s.begin(), s.end(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }));
 }
 
-// trim from end
-static inline std::string &rtrim(std::string &s) {
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace)))
+                         [](unsigned char ch) { return !std::isspace(ch); })
                 .base(),
             s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrim_copy(std::string s) {
+    ltrim(s);
     return s;
 }
 
-// trim from both ends
-static inline std::string &trim(std::string &s) { return ltrim(rtrim(s)); }
+// trim from end (copying)
+static inline std::string rtrim_copy(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trim_copy(std::string s) {
+    trim(s);
+    return s;
+}
 
 std::vector<std::string>
 str_split(const std::string &str, const std::string &delim, bool filter_empty) {
     std::regex re(delim);
     std::sregex_token_iterator first{str.begin(), str.end(), re, -1}, last;
 
-    std::vector<std::string> tmp = {first, last};
-    std::vector<std::string> result;
-    for (auto &split_result : tmp) {
-        result.push_back(trim(split_result));
+    std::vector<std::string> result = {first, last};
+    // trim in place
+    for (auto &split_result : result) {
+        trim(split_result);
     }
     if (filter_empty) {
         result.erase(std::remove_if(result.begin(), result.end(),
