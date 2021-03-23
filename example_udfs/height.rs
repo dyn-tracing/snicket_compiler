9e760c56ea5f97270670f51b8a10d4ea1fbfe6a5
Jessica Berg <jesswberg@gmail.com>
2021-03-23 12:28:29 -0400

    Checkpoint 3

--- example_udfs/height.rs
+++ example_udfs/height.rs
@@ -3,12 +3,12 @@
 // mid_func: mid_height
 // id: height
 
-fn leaf_height(_graph: Graph<(String, IndexMap<String, String>), String>) -> u32 {
+fn leaf_height(_graph: &Graph<(String, IndexMap<String, String>), String>) -> u32 {
     return 0;
 }
 
 // TODO:  must children's responses always be in string form?  can we generalize?
-fn mid_height(_graph: Graph<(String, IndexMap<String, String>), String>, children_responses: Vec<String>) -> u32 {
+fn mid_height(_graph: &Graph<(String, IndexMap<String, String>), String>, children_responses: Vec<String>) -> u32 {
     let mut max = 0;
     for response in children_responses {
         let response_as_u32 = response.parse::<u32>();
