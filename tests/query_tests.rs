88d752efa64c0e8ba085d852b3a3257a4b0817da
fruffy <fruffy@nyu.edu>
2021-04-28 13:51:42 +0200

    Refs

--- tests/query_tests.rs
+++ tests/query_tests.rs
@@ -9,7 +9,7 @@ use test_case::test_case; // Parametrized tests
 #[test_case("height.cql", vec!["height.rs"]; "height")]
 #[test_case("histogram.cql", vec!["histogram.rs"]; "inconclusive - histogram")]
 #[test_case("request_size.cql", vec![]; "request_size")]
-#[test_case("request_size_avg.cql", vec![]; "inconclusive - request_size_avg")]
+#[test_case("request_size_avg.cql", vec!["avg.rs"]; "request_size_avg")]
 #[test_case("return_trace.cql", vec![]; "inconclusive - return_trace")]
 #[test_case("latency.cql", vec!["latency.rs"]; "inconclusive - latency")]
 fn check_compilation_envoy(
@@ -65,7 +65,7 @@ fn check_compilation_envoy(
 #[test_case("height.cql", vec!["height.rs"]; "height")]
 #[test_case("histogram.cql", vec!["histogram.rs"]; "inconclusive - histogram")]
 #[test_case("request_size.cql", vec![]; "request_size")]
-#[test_case("request_size_avg.cql", vec![]; "request_size_avg")]
+#[test_case("request_size_avg.cql", vec!["avg.rs"]; "request_size_avg")]
 #[test_case("request_size_avg_trace_attr.cql", vec![]; "request_size_avg_trace_attr")]
 #[test_case("return_trace.cql", vec![]; "return_trace")]
 #[test_case("latency.cql", vec!["latency.rs"]; "inconclusive - latency")]
